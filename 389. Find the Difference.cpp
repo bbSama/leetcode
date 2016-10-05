ecb4e9b465e808494e175f15de1a1ff7cc31445b
zxbChan <446920281@qq.com>
2016-10-05 14:04:21 +0800

    Create 389. Find the Difference.cpp

---
+++ 389. Find the Difference.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        char res = s[0];
+        for(int i=1;i<s.length();i++)
+            res^=s[i];
+        for(int i=0;i<t.length();i++)
+            res^=t[i];
+        return res;
+    }
+};

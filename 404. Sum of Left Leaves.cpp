2e6b135f862c79fbfdbb36c9b657bb60da106a59
zxbChan <446920281@qq.com>
2016-10-06 15:08:41 +0800

    Update 404. Sum of Left Leaves.cpp

--- 404. Sum of Left Leaves.cpp
+++ 404. Sum of Left Leaves.cpp
@@ -12,11 +12,7 @@ public:
     int sumOfLeftLeaves(TreeNode* root) {
         if(root==NULL)
             return 0;
-        if(root->left==NULL)
-            return sumOfLeftLeaves(root->right);
-        int sum=0;
-        if(root->left->left==NULL&&root->left->right==NULL)
-            sum = root->left->val;
-        return sum+sumOfLeftLeaves(root->left)+sumOfLeftLeaves(root->right);
+        return ( (root->left!=NULL&&root->left->left==NULL&&root->left->right==NULL) ? root->left->val : 0 )
+            + sumOfLeftLeaves(root->left) + sumOfLeftLeaves(root->right);
     }
 };

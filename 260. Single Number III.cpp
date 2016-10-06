4b0eb26351be2457d037df57b2770605d02aee1e
zxbChan <446920281@qq.com>
2016-10-06 14:22:01 +0800

    Create 260. Single Number III.cpp

---
+++ 260. Single Number III.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> singleNumber(vector<int>& nums) {
+        /*map<int,int> m;
+        for(int i=0;i<nums.size();i++)
+            m[nums[i]]++;
+        vector<int> vec;
+        map<int,int>::iterator it = m.begin();
+        for(;it!=m.end();it++)
+            if(it->second==1)
+                vec.push_back(it->first);
+        return vec;*/
+        int diff = accumulate(nums.begin(), nums.end(), 0, bit_xor<int>());
+        /*int diff = 0;
+        for(int i=0;i<nums.size();i++)
+            diff^=nums[i];*/
+        diff &= -diff;
+        vector<int> rets(2, 0);
+        for (int num : nums)
+            rets[!(num & diff)] ^= num;
+        return rets;
+    }
+};

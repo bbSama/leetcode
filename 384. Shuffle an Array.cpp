b951092055a03edbc18e94adbe7b8268e31bcc30
zxbChan <446920281@qq.com>
2016-10-07 10:49:36 +0800

    Create 384. Shuffle an Array.cpp

---
+++ 384. Shuffle an Array.cpp
@@ -0,0 +1,31 @@
+class Solution {
+private:
+    vector<int> nums;
+public:
+    Solution(vector<int> nums) {
+        this->nums = nums;
+    }
+    
+    /** Resets the array to its original configuration and return it. */
+    vector<int> reset() {
+        return nums;
+    }
+    
+    /** Returns a random shuffling of the array. */
+    vector<int> shuffle() {
+        vector<int> res(nums);
+        for(int i=0;i<nums.size();i++)
+        {
+            int j = rand()%(nums.size()-i);
+            swap(res[i],res[j+i]);
+        }
+        return res;
+    }
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(nums);
+ * vector<int> param_1 = obj.reset();
+ * vector<int> param_2 = obj.shuffle();
+ */

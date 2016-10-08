ed4c1f8ded2468d129d9a34804923edba2787110
zxbChan <446920281@qq.com>
2016-10-08 17:22:30 +0800

    Update 378. Kth Smallest Element in a Sorted Matrix.cpp

--- 378. Kth Smallest Element in a Sorted Matrix.cpp
+++ 378. Kth Smallest Element in a Sorted Matrix.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int kthSmallest(vector<vector<int>>& matrix, int k) {
+        // O(n*log(big-small)) solution
         int n = matrix[0].size();
         int small=matrix[0][0],big=matrix[n-1][n-1];
         int mid,cnt,j;
@@ -9,7 +10,7 @@ public:
             mid = (big+small)>>1;
             cnt=0;
             j=n-1;
-            for(int i=0;i<n;i++)
+            for(int i=0;i<n;i++) // Attention: the complexity is 2*n not n**2
             {
                 while(j>=0&&matrix[i][j]>mid) --j;
                 cnt+=j+1;

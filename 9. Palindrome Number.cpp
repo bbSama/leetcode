ebfa75d8092d317e64c3f5812a739b9862a0b834
zxbChan <446920281@qq.com>
2016-11-20 20:17:18 +0800

    Create 9. Palindrome Number.cpp

---
+++ 9. Palindrome Number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0)
+            return false;
+        int y = 0,tmp = x;
+        while(tmp)
+        {
+            y = 10*y + tmp%10;
+            tmp/=10;
+        }
+        return x==y;
+    }
+};

839e37428fbf6d6cfb5b3bc2aedac00bfff6f435
zxbChan <446920281@qq.com>
2016-12-13 14:57:58 +0800

    Update 413. Arithmetic Slices.cpp

--- 413. Arithmetic Slices.cpp
+++ 413. Arithmetic Slices.cpp
@@ -1,22 +1,34 @@
 class Solution {
 public:
     int numberOfArithmeticSlices(vector<int>& A) {
-        if(A.size()<3)
-            return 0;
-        int len = 2, res = 0, i = 1;
-        for(i=1;i<A.size()-1;i++)
+//         if(A.size()<3)
+//             return 0;
+//         int len = 2, res = 0, i = 1;
+//         for(i=1;i<A.size()-1;i++)
+//         {
+//             if(A[i+1]-A[i]!=A[i]-A[i-1])
+//             {
+//                 if(len>=3)
+//                     res += (len-2)*(len-1)/2;
+//                 len = 2;
+//             }
+//             else
+//                 len++;
+//         }
+//         if(len>=3)
+//             res += (len-2)*(len-1)/2;
+//         return res;
+        int curr = 0, res = 0;
+        for(int i=2;i<A.size();i++)
         {
-            if(A[i+1]-A[i]!=A[i]-A[i-1])
+            if(A[i]-A[i-1]==A[i-1]-A[i-2])
             {
-                if(len>=3)
-                    res += (len-2)*(len-1)/2;
-                len = 2;
+                curr++;
+                res += curr;
             }
             else
-                len++;
+                curr = 0;
         }
-        if(len>=3)
-            res += (len-2)*(len-1)/2;
         return res;
     }
 };

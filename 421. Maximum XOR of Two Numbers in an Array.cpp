6a22ceebe0d43c70acc73ec1eec87bcafb4c1331
bbSama <446920281@qq.com>
2016-12-16 16:41:55 +0800

    Create 421. Maximum XOR of Two Numbers in an Array.cpp

---
+++ 421. Maximum XOR of Two Numbers in an Array.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    // still not understand QAQ, btw, why the c++ version much lower than java
+    int findMaximumXOR(vector<int>& nums) {
+        int max = 0, mask = 0;
+        for(int i=31;i>=0;i--)
+        {
+            mask = mask | (1<<i);
+            unordered_set<int> s;
+            for(int num:nums)
+                s.insert(num & mask);
+            int tmp = max | (1<<i);
+            for(int prefix:s)
+            {
+                if(s.find(tmp^prefix)!=s.end())
+                {
+                    max = tmp;
+                    break;
+                }
+            }
+        }
+        return max;
+    }
+};
